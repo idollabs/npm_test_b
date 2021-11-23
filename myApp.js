fe4cece36d4a184a66dab448a260ee29e77a2869
Andrew Dauphinais <andrew.dauphinais@konverse.com>
2021-11-22 18:55:50 -0600

    reverted

--- myApp.js
+++ myApp.js
@@ -2,6 +2,6 @@ console.log('Hello World');
 var express = require('express');
 var app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello Express");
-})
+// app.get("/", (req, res) => {
+//     res.send("Hello Express");
+// })
